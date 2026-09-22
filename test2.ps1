a0d712897651ac563cd662e0d029f9e67c4c7edc
Mario Blanco <34868362+lilwhite@users.noreply.github.com>
2026-09-22 09:12:43 +0200

    Add maintenanceScope projection to graph query

--- test2.ps1
+++ test2.ps1
@@ -2,3 +2,4 @@ az graph query -q "Resources | where type =~ 'microsoft.maintenance/maintenancec
 az graph query -q "Resources | where type contains 'maintenance' | summarize count=count() by type | order by count desc" --first 1000 -o table
 
 az graph query -q "Resources | where type contains 'maintenance' | summarize Total=count() by type | order by Total desc" --first 1000 -o table
+az graph query -q "Resources | where type =~ 'microsoft.maintenance/maintenanceconfigurations' | project name, subscriptionId, resourceGroup, location, maintenanceScope=tostring(properties.maintenanceScope)" --first 1000 -o table
