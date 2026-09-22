502e10329f8fb5bf222d5668d494e0055fac389d
Mario Blanco <34868362+lilwhite@users.noreply.github.com>
2026-09-22 09:10:19 +0200

    Add query to summarize maintenance resource types

--- test2.ps1
+++ test2.ps1
@@ -1,1 +1,2 @@
 az graph query -q "Resources | where type =~ 'microsoft.maintenance/maintenanceconfigurations' | project name, subscriptionId, resourceGroup, location, properties.maintenanceScope" --first 1000 -o table
+az graph query -q "Resources | where type contains 'maintenance' | summarize count=count() by type | order by count desc" --first 1000 -o table
